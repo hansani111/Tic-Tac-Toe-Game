c13b84cad64b83091e098842ee8df19b924c6c58
hansani111 <hansanideshmukh894@gmail.com>
2023-01-04 23:49:00 +0530

    added: check free space

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,57 +3,56 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-    static char [] board = new char[10];
+    static char[] board = new char[10];
     static char playerSymbol;
     static char computerSymbol;
 
-
     static void createBoard() {
-        for(int i=1; i<board.length; i++) {
+        for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
         System.out.println("board is created");
     }
 
-    static void assignSymbol(){
+    static void assignSymbol() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose your letter (X or O): ");
         playerSymbol = sc.next().charAt(0);
-        if(playerSymbol == 'X' || playerSymbol == 'x'){
+        if (playerSymbol == 'X' || playerSymbol == 'x') {
             computerSymbol = 'O';
-        }else if(playerSymbol == 'O' || playerSymbol == 'o'){
+        } else if (playerSymbol == 'O' || playerSymbol == 'o') {
             computerSymbol = 'X';
-        }else{
+        } else {
             System.out.println("Invalid input");
             assignSymbol();
         }
     }
 
-    static void showBoard(){
-        System.out.println(board[1] +" | "+ board[2] + " | " + board[3]);
+    static void showBoard() {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("----------");
-        System.out.println(board[4] +" | "+ board[5] + " | " + board[6]);
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("----------");
-        System.out.println(board[7] +" | "+ board[8] + " | " + board[9]);
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
         System.out.println("********************************************");
     }
 
-    static void userInput(){
+    static void userInput() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose the position b/w (1 to 9): ");
         int user = sc.nextInt();
-        if(user > 9 || user < 1){
+        if (user > 9 || user < 1) {
             System.out.println("Invalid input: ");
             userInput();
-        }
-        else if(board[user] == ' '){
+        } else if (board[user] == ' ') {
             board[user] = playerSymbol;
-        }else{
+            showBoard();
+        } else {
             System.out.println("Its not Empty");
             userInput();
         }
-
     }
+
     public static void main(String[] args) {
         System.out.println("-----Welcome to tic tac toe problems-----");
         createBoard();
